3a4a11b172105fc3840342d4f9677c30911bd3ea
rezaEcho <[200~rezah1377m@gmail.comgit config --global user.email rezah1377m@gmail.comgit config --global user.email [200~rezah1377m@gmail.com>
2023-07-28 17:15:38 +0330

    AddEmployee

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import './style.css'
 import Login from './Components/Login'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Dashboard from './Components/Dashboard'
+import Employee from './Components/Employee'
+import Profile from './Components/Profile'
+import Home from './Components/Home'
+import AddEmployee from './Components/AddEmployee'
 
 function App() {
 
   return (
-        <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Dashboard />}>
+              <Route path='' element={<Home />} />
+              <Route path='/employee' element={<Employee />} />
+              <Route path='/create' element={<AddEmployee />} />
+              <Route path='/profile' element={<Profile />} />
+          </Route>
+        <Route path='/Login' element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
       )
 }
 
